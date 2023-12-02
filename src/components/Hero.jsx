604b27528c39a773698d3653d5865d53ee4851d3
Ashish Prakash <asoftwareguy11@gmail.com>
2023-12-02 17:31:37 +0530

    initialized the project with two components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+import React from "react";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+
+const Hero = () => {
+  return (
+    <div className="grid grid-cols-4 p-2 m-2">
+      <div className="md:p-1 md:m-1 col-span-4 md:col-span-2">
+        <h2 className="text-4xl font-semibold mb-6 md:px-2 mx-2">
+          Capture <br />
+          the Taste <br /> with <span className="text-primary">FoodShoot</span>
+          : <br /> Order Online!
+        </h2>
+        <p className="text-base font-light text-gray-200 mb-6 md:px-2 ml-2 md:mr-6">
+          Embark on a culinary journey with FoodShoot! Explore a diverse menu
+          crafted with passion. Order now for an experience that brings vibrant
+          Indian tastes to your doorstep!
+        </p>
+        <div className="flex flex-wrap gap-4 md:p-2 m-2 items-center">
+          <Button
+            color="primary"
+            variant="shadow"
+            size="lg"
+            className="uppercase"
+            endContent={<IoArrowForwardCircleOutline />}
+          >
+            Order Now
+          </Button>
+          <Button
+            color="primary"
+            variant="bordered"
+            size="lg"
+            endContent={<IoArrowForwardCircleOutline />}
+          >
+            Learn More
+          </Button>
+        </div>
+      </div>
+      <div className="relative hidden md:block col-span-2 ">
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,110 @@
+"use client";
+import {
+  Button,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
+import React from "react";
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Home",
+    "Menu",
+    "About",
+    "Contact",
+    "Profile",
+    "Cart",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="sm:hidden px-4" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <span className="text-primary font-semibold text-3xl">
+            FoodShoot{" "}
+          </span>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4 " justify="center">
+        <NavbarBrand>
+          <span className="text-primary font-semibold text-3xl">
+            FoodShoot{" "}
+          </span>
+        </NavbarBrand>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Menu
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Profile
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="warning" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu className="dark">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? "warning"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
